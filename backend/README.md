03b97550bddac6307156ef4b87a47e09b2b163ff
Aby Sebastian <abysebastian98@gmail.com>
2023-01-15 22:41:44 +0530

    installed django-cors-headers and restructured folder structure

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# Django_JWT_auth
+A project created to learn JWT authentication in django rest framework using simpleJWT.
+ [x] - install and use simpleJWT
+ [x] - blacklist previously used access token
+ [ ] - install django-cors-headers to manage cors errors
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -92,6 +93,9 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",     # added for django-cors-headers
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -171,3 +175,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
