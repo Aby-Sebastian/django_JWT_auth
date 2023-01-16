69f59dcdcc80fcd26ac8db388986deee6599e194
Aby Sebastian <abysebastian98@gmail.com>
2023-01-16 23:06:32 +0530

    setup basic react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import { BrowserRouter as Router,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-
+import  Header  from './components/Header'
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route component={HomePage} path="/" exact/> 
-        <Route component={LoginPage} path="/login/"/>
+        <Header />
+        <Routes>      {/* as of v6.0 things won't work if Routes are not added */}
+          <Route element={<HomePage />} path="/" exact/> 
+          <Route element={<LoginPage />} path="/login/"/>
+        </Routes>
       </Router>
     </div>
   );
