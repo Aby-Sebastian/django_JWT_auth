61e6590c1515a42014e4f931e7dac065ff56d7cd
Aby Sebastian <abysebastian98@gmail.com>
2023-01-17 23:24:33 +0530

    PrivateRoute file created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import  Header  from './components/Header'
+import PrivateRoute from './utils/PrivateRoute'
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <Routes>      {/* as of v6.0 things won't work if Routes are not added */}
-          <Route element={<HomePage />} path="/" exact/> 
-          <Route element={<LoginPage />} path="/login/"/>
-        </Routes>
+              {/* as of v6.0 things won't work if Routes are not added */}
+          <PrivateRoute element={<HomePage />} path="/" exact/> 
+          <PrivateRoute element={<LoginPage />} path="/login/"/>
+        
       </Router>
     </div>
   );
