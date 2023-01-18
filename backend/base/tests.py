89ce0abbd87ae9755854dbae91b6a2e6a8e56aa8
Aby Sebastian <abysebastian98@gmail.com>
2023-01-18 21:57:17 +0530

    added tests for backend urls

--- backend/base/tests.py
+++ backend/base/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class URLTestCase(TestCase):
+    def test_home(self):
+        response = self.client.get('/api/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_token(self):
+        response = self.client.get('/api/token/')
+        self.assertEqual(response.status_code, 405)
+
+    def test_token_refresh(self):
+        response = self.client.get('/api/token/refresh/')
+        self.assertEqual(response.status_code, 405)
