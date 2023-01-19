07b3189d401bcad59005e71d4196bc1a650861dc
Aby Sebastian <abysebastian98@gmail.com>
2023-01-19 23:42:48 +0530

    updated the code react router v6.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import  Header  from './components/Header'
-import PrivateRoute from './utils/PrivateRoute'
+import PrivateRoutes from './utils/PrivateRoute'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Routes>
               {/* as of v6.0 things won't work if Routes are not added */}
-          <PrivateRoute element={<HomePage />} path="/" exact/> 
-          <PrivateRoute element={<LoginPage />} path="/login/"/>
-        
+          <Route element={<PrivateRoutes />}>
+            <Route element={<HomePage />} path="/" exact/> 
+            <Route element={<LoginPage />} path="/login/"/>
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
